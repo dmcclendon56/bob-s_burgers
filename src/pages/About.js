987881937b9e53335ff9771d97182df2f6cb1682
dmcclendon56 <mcclendon.darrian@gmail.com>
2022-05-20 07:36:28 -0500

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../componets/Header'
+import "./CharacterPage.css"
 
 function About(props) {
   return (
     <div>
-        <h3>Bob's Burgers </h3>
+        <h2 className='title'>Bob's Burgers Blurb</h2>
+        <h4 className='blurb'>An animated sitcom about a lax burger restaurant 
+          owner and his eccentric family including his wife, two daughters, and son. The Belcher family is all but normal. There is never a dull day in the restaurant.
+          These parents struggle to get by, and their kids do all but make it easy for them.</h4>
     </div>
   )
 }

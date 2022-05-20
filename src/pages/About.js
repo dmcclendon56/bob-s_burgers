3e112b3c1f6b2d4ce5afc3f651a16bf8cdd4d83e
dmcclendon56 <mcclendon.darrian@gmail.com>
2022-05-20 07:58:20 -0500

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,6 +5,7 @@ import "./CharacterPage.css"
 function About(props) {
   return (
     <div>
+      <img className='family' src='https://www.billboard.com/wp-content/uploads/media/bobs-burgers-2017-billboard-1548.jpg' alt='family portrait'></img>
         <h2 className='title'>Bob's Burgers Blurb</h2>
         <h4 className='blurb'>An animated sitcom about a lax burger restaurant 
           owner and his eccentric family including his wife, two daughters, and son. The Belcher family is all but normal. There is never a dull day in the restaurant.
